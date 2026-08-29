589669ce2b570607f7a8e8acde7bc2d4d1f4be2c
Prince <nkosana.connect@gmail.com>
2026-08-29 07:58:58 +0200

    Feat: Implement volunteer basic structure.

--- src/main/java/com/prince/model/Volunteer.java
+++ src/main/java/com/prince/model/Volunteer.java
@@ -1,4 +1,38 @@
 package com.prince.model;
 
-public class Volunteer {
+import com.prince.database.DataBaseConnector;
+
+public class Volunteer extends Person{
+
+    private final DataBaseConnector DATABASE;
+
+    public Volunteer(String name, String idNumber) {
+        super(name, idNumber);
+        this.DATABASE = new DataBaseConnector();
+    }
+
+    public boolean addPickUpPoint(PickUpPoint store) {
+        try {
+            DATABASE.addStork(store.getName(), store.getAddress());
+            return true;
+
+        } catch (IllegalArgumentException){
+            return false;
+        }
+    }
+
+    public boolean addCrates(String pickUpPointName, int crates) {
+        try {
+            DATABASE.addStork(pickUpPointName, crates);
+            return true;
+
+        } catch (IllegalArgumentException){
+            return false;
+        }
+    }
+
+    public void viewCapacities() {
+
+    }
+
 }
