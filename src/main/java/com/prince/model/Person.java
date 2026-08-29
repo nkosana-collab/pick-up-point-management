4f042a1863d499f186ab9b48be0e0535ca261b2c
Prince <nkosana.connect@gmail.com>
2026-08-29 07:18:27 +0200

    Feat: Implement the Person abstract class.

--- src/main/java/com/prince/model/Person.java
+++ src/main/java/com/prince/model/Person.java
@@ -1,4 +1,19 @@
 package com.prince.model;
 
-public class Person {
+public abstract class Person {
+    private String name;
+    private String inNumber;
+
+    public Person(String name, String idNumber) {
+        this.name = name;
+        this.inNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInNumber() {
+        return inNumber;
+    }
 }
