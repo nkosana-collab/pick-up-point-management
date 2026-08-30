c934b0e53c991bafff233ec11e52492cba557350
Prince <nkosana.connect@gmail.com>
2026-08-30 18:56:59 +0200

    Test: Add VolunteerAddsCratesMultipleTimesSuccessfully() test to VolunteerTest.java

--- src/test/java/com/prince/unittesting/VolunteerTest.java
+++ src/test/java/com/prince/unittesting/VolunteerTest.java
@@ -1,14 +1,11 @@
 package com.prince.unittesting;
 
-import com.prince.model.PickUpPoint;
+
 import com.prince.model.Volunteer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
