9b857496cac638899918aa189cf52eb6da768b42
Prince <nkosana.connect@gmail.com>
2026-08-30 18:55:11 +0200

    Test: Add VolunteerAddsCratesMultipleTimesSuccessfully() test to VolunteerTest.java

--- src/test/java/com/prince/unittesting/VolunteerTest.java
+++ src/test/java/com/prince/unittesting/VolunteerTest.java
@@ -1,8 +1,12 @@
 package com.prince.unittesting;
 
+import com.prince.model.PickUpPoint;
 import com.prince.model.Volunteer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * A well declared Volunteer object must have a valid name and ID,
  * it must have the ability to query and update the database.
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class VolunteerTest {
 
     private final String URL = "jdbc:sqlite:memory";
@@ -32,4 +37,23 @@ public class VolunteerTest {
         assertTrue(result);
     }
 
+    /**
+     * Given that I have a valid Volunteer Object with database connection,
+     * When I add a new PickUpPoint into the database--
+     * Then I should be allowed to do so and add crates as many times as legal to that PickUpPoint.
+     */
+    @Test
+    void VolunteerAddsCratesMultipleTimesSuccessfully() {
+        // Given:
+        Volunteer volunteer = new Volunteer("Siyabonga", "99449", URL);
+        // When:
+        boolean addResult = volunteer.addPickUpPoint("Sbindi", "vosman");
+        // The:
+        assertTrue(addResult);
+
+        // Multiple additions of crates to the newly added PickUpPoint.
+        for(int i = 1; i < 5; i++) {
+            assertTrue(volunteer.addCrates("Sbindi",i));
+        }
+    }
 }
